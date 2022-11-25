c363b37b2189851f5ae224e6d38f55da2288fed1
lem <lilililili@yandex.ru>
2022-11-25 18:22:04 +0400

    pygame, target

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ import pygame
 MAXW, MAXH = 1000, 800
 size = width, height = None, None
 
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+
 
 def draw(cur_screen):
     screen.fill((255, 255, 255))
-    side = a // n
-    for i in range(n):
-        for j in range(n):
-            if (i + j) % 2 == 0:
-                print(i, j)
-                screen.fill((0, 0, 0), (i * side, (height - side - j * side), side, side))
+    for i in range(n, 0, -1):
+        pygame.draw.circle(cur_screen, colors[(i - 1) % 3], (width // 2, height // 2), i * w)
 
 
 if __name__ == '__main__':
 
-    a, n = map(int, input().split())
-    size = wight, height = a, a
+    w, n = map(int, input().split())
+    size = width, height = 2 * w * n, 2 * w * n
     pygame.init()
     screen = pygame.display.set_mode(size)
     draw(screen)
