72a4772361144307334e0da13b4f994522f34a4b
lem <lilililili@yandex.ru>
2022-12-02 17:01:37 +0400

    board v 0.2

--- main.py
+++ main.py
@@ -4,27 +4,37 @@ size = width, height = 800, 400
 
 
 class Board:
-    # создание поля
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        # значения по умолчанию
+
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.board = [[0] * m for _ in range(n)]
+
         self.left = 10
         self.top = 10
         self.cell_size = 30
+        self.color = pygame.Color('purple')
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
+    def set_view(self, left, top, cell_size, color=pygame.Color('purple')):
         self.left = left
         self.top = top
         self.cell_size = cell_size
+        self.color = color
+
+    def draw(self, cur_screen):
+        for i in range(self.n):
+            for j in range(self.m):
+                pygame.draw.rect(cur_screen, self.color,
+                                 (self.left + j * self.cell_size, self.top + i * self.cell_size,
+                                  self.cell_size + 1, self.cell_size + 1), 1)
 
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode(size)
 
+    board = Board(3, 5)
+    board.set_view(50, 20, 100)
     clock = pygame.time.Clock()
     fps = 30
     running = True
@@ -35,6 +45,7 @@ if __name__ == '__main__':
             elif event.type == pygame.MOUSEBUTTONUP:
                 pass
         screen.fill((250, 255, 200))
+        board.draw(screen)
         pygame.display.flip()
         clock.tick(fps)
     pygame.quit()
