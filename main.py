53fd558b9a9e06f432b5b8354fbbbd5444d7ab5c
lem <lilililili@yandex.ru>
2022-12-09 18:14:20 +0400

    bombs rain

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import random
 
 import pygame
 
 size = width, height = 1000, 600
+pygame.init()
+screen = pygame.display.set_mode(size)
 
 
 def load_image(name, color_key=None):
@@ -22,21 +25,36 @@ def load_image(name, color_key=None):
     return image
 
 
+class Bomb(pygame.sprite.Sprite):
+    img1 = load_image("bomb.png")
+    img2 = load_image("boom.png")
+
+    def __init__(self, pos, *group):
+        super().__init__(*group)
+        self.image = Bomb.img1
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos[0] - self.rect.width // 2, pos[1] - self.rect.height // 2
+
+    def update(self, *args):
+        if args:
+            if args[0].type == pygame.MOUSEBUTTONUP and args[0].button != 1:
+                if self.image == Bomb.img1 and self.rect.collidepoint(args[0].pos):
+                    self.image = Bomb.img2
+                    self.rect.width = self.image.get_rect().width
+                    self.rect.height = self.image.get_rect().height
+                    self.rect.x -= self.rect.width // 4
+                    self.rect.y -= self.rect.height // 4
+        else:
+            self.rect = self.rect.move(random.randrange(3) - 1,
+                                       random.randrange(3) - 1)
+
+
+
 if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode(size)
 
     img1 = load_image("asteroid.png")
-    img2 = load_image("bomb.png")
 
     all_sprites = pygame.sprite.Group()
-    pers = pygame.sprite.Sprite(all_sprites)
-    pers.image = img2
-    pers.rect = pers.image.get_rect()
-    pers.rect.x = 10
-    pers.rect.y = 10
-    pers.dx = 0
-    pers.dy = 0
 
     clock = pygame.time.Clock()
     fps = 60
@@ -46,32 +64,21 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONUP:
-                pers.rect.x = event.pos[0] - pers.rect.width // 2
-                pers.rect.y = event.pos[1] - pers.rect.height // 2
+                if event.button == 1:
+                    Bomb(event.pos, all_sprites)
+                else:
+                    all_sprites.update(event)
             elif event.type == pygame.MOUSEMOTION:
                 pass
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    pers.dy -= 1
-                elif event.key == pygame.K_DOWN:
-                    pers.dy += 1
-                elif event.key == pygame.K_LEFT:
-                    pers.dx -= 1
-                elif event.key == pygame.K_RIGHT:
-                    pers.dx += 1
+                pass
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    pers.dy += 1
-                elif event.key == pygame.K_DOWN:
-                    pers.dy -= 1
-                elif event.key == pygame.K_LEFT:
-                    pers.dx += 1
-                elif event.key == pygame.K_RIGHT:
-                    pers.dx -= 1
-        print(pers.dx, pers.dy)
+                pass
+
         screen.fill((50, 20, 75))
-        pers.rect = pers.rect.move(pers.dx, pers.dy)
+
         all_sprites.draw(screen)
         pygame.display.flip()
+        all_sprites.update()
         clock.tick(fps)
     pygame.quit()
