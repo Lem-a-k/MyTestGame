e45ff6daad5ee5d1be4ee472a16b595be6923f5c
lem <lilililili@yandex.ru>
2022-12-16 17:22:40 +0400

    menu v 0.1

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ screen = pygame.display.set_mode(size)
 
 BORDER = 5
 
+MENU_TXT = ["Начать", "Правила игры", "Выход"]
+
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
@@ -111,29 +113,57 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     fps = 60
     running = True
+    stage = 'menu'
+    selector = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    Bomb(event.pos, all_sprites, all_bombs)
-                else:
-                    all_sprites.update(event)
-            elif event.type == pygame.MOUSEMOTION:
-                pass
+                if stage == 'menu':
+                    pass
+                elif stage == 'game':
+                    if event.button == 1:
+                        Bomb(event.pos, all_sprites, all_bombs)
+                    else:
+                        all_sprites.update(event)
             elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-                all_sprites.update(event)
+                if stage == 'game':
+                    if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                        stage = 'menu'
+                    all_sprites.update(event)
+                elif stage == 'menu':
+                    if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                        if selector == 0:
+                            stage = 'game'
+                        elif selector == len(MENU_TXT) - 1:
+                            running = False
+                    elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+                        selector = min(len(MENU_TXT) - 1, selector + 1)
+                    elif event.type == pygame.KEYUP and event.key == pygame.K_UP:
+                        selector = max(0, selector - 1)
 
-        screen.fill((50, 20, 75))
+        if stage == 'menu':
+            screen.fill(pygame.Color('coral4'))
+            font = pygame.font.Font(None, 30)
 
-        all_bombs.update(main_character)
-        # pygame.sprite.groupcollide(main_character, all_bombs, False, True)
+            for idx, line in enumerate(MENU_TXT):
+                string_rendered = font.render(line, 1, pygame.Color('white'))
+                intro_rect = string_rendered.get_rect()
+                intro_rect.top = 40 + idx * 20
+                intro_rect.x = width // 2 - intro_rect.width // 2
+                screen.blit(string_rendered, intro_rect)
+                if idx == selector:
+                    pygame.draw.rect(screen, pygame.Color('white'), intro_rect, 1)
 
-        all_bombs.draw(screen)
-        main_character.draw(screen)
+        elif stage == 'game':
+            screen.fill((50, 20, 75))
+            all_bombs.update(main_character)
+            # pygame.sprite.groupcollide(main_character, all_bombs, False, True)
+            all_sprites.update()
+            all_bombs.draw(screen)
+            main_character.draw(screen)
 
         pygame.display.flip()
-        all_sprites.update()
         clock.tick(fps)
     pygame.quit()
