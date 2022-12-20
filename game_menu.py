909408f753e783939d7413fe690b9ba80658977f
lem <lilililili@yandex.ru>
2022-12-20 18:07:45 +0400

    Menu + game v0.1

--- game_menu.py
+++ game_menu.py
@@ -10,6 +10,7 @@ screen = pygame.display.set_mode(size)
 if __name__ == "__main__":
     running = True
     game = None
+    font = pygame.font.Font(None, 30)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,5 +18,10 @@ if __name__ == "__main__":
             if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
                 running = game_main()
         screen.fill(pygame.Color("yellow"))
+        string_rendered = font.render("Нажмите <пробел>", 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        intro_rect.top = 40
+        intro_rect.x = width // 2 - intro_rect.width // 2
+        screen.blit(string_rendered, intro_rect)
         pygame.display.flip()
     pygame.quit()
